dc7e1e61fc3ae82a293fd7c2221d9fbc9654c388
Antony Lemos <lemosantony@gmail.com>
2020-10-08 18:44:49 -0300

    🚧 angular/chore: create dashboard page

--- gowww-angular/src/app/app.module.ts
+++ gowww-angular/src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/Dashboard/dashboard.component';
+
+import { NavbarComponent } from './components/Navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
